c5d52b0a0641be944040f365526d1206cf22c2b7
Thinh Nguyen <rinktvn2525@gmail.com>
2023-08-18 16:55:22 +0700

    update menu api

--- src/services/shopService.js
+++ src/services/shopService.js
@@ -20,13 +20,21 @@ export const getShopInfo = async (latitude = 10.848046, longitude = 106.785888) 
     }
 };
 export const getItemFromShop = async (idType) => {
-    const config = {
-        params: {
-            idType,
-        },
-    };
+    // const config = {
+    //     params: {
+    //         idType,
+    //     },
+    // };
     try {
-        const res = await httpRequest.get('aa790e39-70d5-464e-a7e9-71f4d478a915', config);
+        const res = await httpRequest.get(
+            idType === 1
+                ? 'aa790e39-70d5-464e-a7e9-71f4d478a915'
+                : idType === 2
+                ? '8ca65c49-023e-4666-9900-0edfcf5e31d3'
+                : idType === 3
+                ? '76605832-e7cd-47a8-b0ac-f962c4c32f6f'
+                : '6b7701e8-6f35-4032-9c71-ed560fd67112',
+        );
         return res.data;
     } catch (error) {
         console.log(error);
